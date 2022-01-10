0932729dc04fb4518004a4bb0320164581a23e73
Alexander Lobas <alexanderlobas@yahoo.com>
2022-01-10 15:36:18 +0200

    stack size problem

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-//import '../graphQLTypes';
+import '../graphQLTypes';
 
 export default function TabTwoScreen() {
   return (
